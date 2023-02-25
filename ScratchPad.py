e88c314a53432c5000b7af5286884ae6021ed6a6
Vyshnav Prasad <vyshnav.prasad@outlook.com>
2023-02-25 13:24:05 +0530

    Reefer ScratchPad

---
+++ ScratchPad.py
@@ -0,0 +1,45 @@
+import sys
+import time as tim
+import shutil
+import keyboard as key
+
+# Initialize
+consoleWidth, consoleheight = shutil.get_terminal_size()
+symbols = ["cherry", "diamond", "heart", "banana", "chappal", "coffee", "tree"]
+Lsymbols = len(symbols)
+
+
+def update(text):  # whatever text is given gets displayed in center of terminal
+    global consoleWidth
+    x = text.center(consoleWidth)
+    print("\r", end="")
+    print(x, end="")
+    tim.sleep(.1)
+
+def stringBuild()
+
+key.wait('enter')
+for i in range(10):
+    update(symbols[i%Lsymbols])
+    
+# sys.stdout.write('hello')
+# sys.stdout.flush()
+# for _ in range(5):
+#     time.sleep(1)
+#     sys.stdout.write('\033[D \033[D')
+#     sys.stdout.flush()
+
+# print("boss")
+# time.sleep(1)
+# sys.stdout.flush()
+
+
+# for i in range(20):
+#     print(symbols[i%Lsymbols], end="")
+#     # sys.stdout.flush()
+#     print("\r",end="")
+#     time.sleep(.05)
+
+# for x in range(10):
+#     print("Grape \r",end="")
+
