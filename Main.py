6d59c19ccf8ec986f2c1dd12a5a2dc4581a63c6d
Svenski-79 <84986379+Svenski-79@users.noreply.github.com>
2023-02-27 00:53:23 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,44 +8,77 @@ import time as tim
 import random
 import keyboard as key
 import shutil
-import threading 
 
-consoleWidth, consoleheight = shutil.get_terminal_size() #calculates terminal height and width 
+consoleWidth, consoleheight = shutil.get_terminal_size(
+)  #calculates terminal height and width
 
 
-def gameDisp(text):     #whatever text is given gets displayed in center of terminal
+def typed(text) -> str:
+  global consoleWidth
+  para = text.center(consoleWidth)
+  for i in para:
+    print(i, end='', flush=True)
+    tim.sleep(0.05)
+  # print('\n')
+
+
+def gameDisp(text):  # centralized text
   global consoleWidth
   x = text.center(consoleWidth)
-  print(x, sep=" ", end="\t")
+  print(x, sep=" ")
 
-symbols = {"cherry":4,
-           "diamond":5,
-           "heart":3,
-           "banana":2,
-           "chappal":1,
-           "coffee":2,
-           "tree":1,
-           "X":0}
+
+def gameInput(text):
+  global consoleWidth
+  leftMargin = (consoleWidth - len(text)) // 2 - 6
+  x = " " * leftMargin + text
+  Input = input(x)
+  return Input
+
+
+symbols = {
+  "cherry": 4,
+  "diamond": 5,
+  "heart": 3,
+  "banana": 2,
+  "chappal": 1,
+  "coffee": 2,
+  "tree": 1,
+  "X": 0
+}
 imageReel = []
 
 
-def hype() -> None:   #some text to keep them in
+def hype() -> None:  #some text to keep them in
   tim.sleep(1)
   print("Get ready with your gambling Addiction")
 
 
-def denominator(factor, money): #gambling term just google
+def greedCheck(fee):
+  if int(fee) > 1000:
+    print("\n")
+    typed("I might have to see your bank balance are you for real?!")
+    typed("Do you really have that much money?(y/n)")
+    result = gameInput("> ")
+
+    if result == "y" or result == "yes" or "yes" in result: 
+      return True
+    else:
+      typed("Ok bruh, enter a 'reasonable' amount")
+
+
+def denominator(factor, money):  #gambling term just google
   credit = money / factor
   return credits
 
 
-def slot() -> str:    #generates 1 symbol for the possible 3 slots 
+def slot() -> str:  #generates 1 symbol for the possible 3 slots
   global symbols
-  slots = random.choice(symbols,weights=(42,10,42,30,35,28,33,40))
+  slots = random.choice(symbols, weights=(42, 10, 42, 30, 35, 28, 33, 40))
   return slots
 
 
-def cursorSpin(time)->None:    #A spinning cursor animation,it spits None when animation is over ,need to fix that
+def cursorSpin(time) -> None:  #spin cursor
 
   def spinning_cursor():
     while True:
@@ -53,26 +86,37 @@ def cursorSpin(time)->None:    #A spinning cursor animation,it spits None when a
         yield cursor
 
   spinner = spinning_cursor()
-  for i in range (time):
+  for i in range(time):
     for _ in range(4):
       sys.stdout.write(next(spinner))
       sys.stdout.flush()
       tim.sleep(0.1)
       sys.stdout.write('\b')
-  
 
 
-def startMachine() -> None:    #displays the symbol       
+def startMachine() -> None:  #displays the symbol
   gameDisp("--------------------------")
-  reel1 = slot()
-  reel2=slot()
-  reel3 = slot()
-  
 
   slot()
   gameDisp("--------------------------")
+  return
 
-def startGame():    #starts game on enter
+
+def startGame():  #starts game on enter
   while True:
-    key.wait('enter')    
-    startMachine()
+    typed("Hey what do you go by, I mean your name...?")
+    name = gameInput("> ")
+    typed(f"Hi {name}")
+
+    while True:
+      typed("How much money would you like to deposit : ")
+      deposit = gameInput("> $")
+      result = greedCheck(deposit)
+      if result == True:
+        print("")
+        break
+      
+    # key.wait('enter')
+
+
+startGame()
